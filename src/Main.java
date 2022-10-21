13f2e7e391a2bcc074d0e433103b363724c1022d
Irina <irakali86@gmail.com>
2022-10-21 19:30:44 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 25000;
+        int bonus = 20;
+        int mili = price / bonus;
+        System.out.println(mili);
+
+    }
+}
